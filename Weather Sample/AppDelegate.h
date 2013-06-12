91dfd775b456926c34de2159ef7f347af8a953f4
tleblond@bsoft.fr <tleblond@bsoft.fr>
2013-06-12 14:44:40 +0200

    Initial Commit

---
+++ Weather Sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather Sample
+//
+//  Created by Thomas Leblond on 12/06/13.
+//  Copyright (c) 2013 Thomas LEBLOND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Weather Sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Weather Sample
+//
+//  Created by Thomas Leblond on 12/06/13.
+//  Copyright (c) 2013 Thomas LEBLOND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
